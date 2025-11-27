35fd61224d1a58a7f384655de9a5f1c16823d823
bishu-ebl <bishu.ebl@gmail.com>
2025-11-27 19:50:47 +0600

    Homepage setup, postgress configuration, .env-sample

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello World')
+    #return HttpResponse('Hello World')
+    return render(request, 'home.html') # Create a folder templates and crete home.html in this folder. Later confiugure the template folder in settings.py
