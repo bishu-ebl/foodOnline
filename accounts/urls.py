929a63b25f54ad131e1701ccb70a21352639e9e0
bishu-ebl <bishu.ebl@gmail.com>
2025-12-11 16:14:52 +0600

    Forgot Password Featurea

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,4 +13,11 @@ urlpatterns=[
     #path('dashboard/', views.dashboard, name='dashboard'),
     path('custDashboard/', views.custDashboard, name='custDashboard'), 
     path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'), 
+
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+
+    # Path to reset the password
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('reset_password_validate/<uidb64>/<token>', views.reset_password_validate, name='reset_password_validate'),
+    path('reset_password/', views.reset_password, name='reset_password'),
 ]
